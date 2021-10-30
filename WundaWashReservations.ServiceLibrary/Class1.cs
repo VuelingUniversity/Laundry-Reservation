d22b878a0a857a9e156ef0a3ff42fd602b738255
Selene <selene.ajtd@gmail.com>
2021-10-30 16:42:19 +0200

    First approximation of the WundaWashReservation structure.

---
+++ WundaWashReservations.ServiceLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WundaWashReservations.ServiceLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ WundaWashReservations.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WundaWashReservations.WebApi.WebApiApplication" Language="C#" %>
